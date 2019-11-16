985e582e8a72c60f56ff8b832e3b3f1b5f2c37ec
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2019-11-16 17:05:38 +0000

    Add search Fe to landing page

--- src/components/Headers/IndexHeader.js
+++ src/components/Headers/IndexHeader.js
@@ -1,26 +1,23 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 /*eslint-disable*/
 import React from "react";
 
 // reactstrap components
 import { Container } from "reactstrap";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  Form,
+  Input,
+  InputGroup,
+  CardTitle,
+  CardBody,
+  InputGroupAddon,
+  InputGroupText, 
+  Row,
+  Col
+} from "reactstrap";
 
 // core components
 
@@ -34,42 +31,46 @@ function IndexHeader() {
             "url(" + require("assets/img/antoine-barres.jpg") + ")"
         }}
       >
-        <div className="filter" />
-        <div className="content-center">
-          <Container>
-            <div className="title-brand">
-              <h1 className="presentation-title">Paper Kit React</h1>
-              <div className="fog-low">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
-              </div>
-              <div className="fog-low right">
-                <img alt="..." src={require("assets/img/fog-low.png")} />
-              </div>
-            </div>
-            <h2 className="presentation-subtitle text-center">
-              Make your mark with a Free Bootstrap 4 (Reactstrap) UI Kit!
-            </h2>
-          </Container>
-        </div>
-        <div
-          className="moving-clouds"
-          style={{
-            backgroundImage: "url(" + require("assets/img/clouds.png") + ")"
-          }}
-        />
-        <h6 className="category category-absolute">
-          Designed and coded by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=pkr-index-page"
-            target="_blank"
-          >
-            <img
-              alt="..."
-              className="creative-tim-logo"
-              src={require("assets/img/creative-tim-white-slim2.png")}
-            />
-          </a>
-        </h6>
+        <Container>
+        <Row>
+              <Col className="ml-auto mr-auto" md="8">
+                <h2 className="text-center">Search</h2>
+                <Form className="contact-form">
+                  <Row>
+                    <Col md="6">
+                      {/* <label>City</label> */}
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="nc-icon nc-single-02" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input placeholder="City" type="text" />
+                      </InputGroup>
+                    </Col>
+                    <Col md="6">
+                      {/* <label>Number of Desks</label> */}
+                      <InputGroup>
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                          <i className="nc-icon nc-single-02" />
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input placeholder="Number of Desks" type="text" />
+                      </InputGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="ml-auto mr-auto" md="4">
+                      <Button style={{width: "100%"}} className="btn-fill" color="danger" size="lg" >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
+                </Form>
+              </Col>
+            </Row>
+        </Container>
       </div>
     </>
   );
